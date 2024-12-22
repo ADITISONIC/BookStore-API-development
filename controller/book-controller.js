8100ad8c04c1ad19bf1e81dc7bed64b1ccfcc77f
iaditi <aditipython@gmail.com>
2024-12-22 15:07:00 +0530

    changes added

---
+++ controller/book-controller.js
@@ -0,0 +1,16 @@
+const { get } = require("mongoose");
+
+const getAllBooks = async (req, res) => {};
+
+const getSingleBook = async (req, res) => {};
+const addNewBook = async (req, res) => {};
+const deleteBook = async (req, res) => {};
+const updateBook = async (req, res) => {};
+
+module.exports = {
+  getAllBooks,
+  getSingleBook,
+  addNewBook,
+  updateBook,
+  deleteBook,
+};
